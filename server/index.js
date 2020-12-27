452f3f77b280f022b6f5c8144ebd171abf42ded1
Harry Chen <harrychen0314@gmail.com>
2020-12-27 10:02:15 -0800

    rename problems to questions

--- server/index.js
+++ server/index.js
@@ -7,11 +7,11 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-const problems = require('./routes/api/problems');
-app.use('/api/problems', problems);
+const problems = require('./routes/api/questions');
+app.use('/api/questions', problems);
 
 const heartbeat = require('./routes/api/heartbeat');
 app.use('/api/heartbeat',heartbeat);
 
 const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`Backend Server started on port ${port}`));
+app.listen(port, () => console.log(`Server started on port ${port}`));
