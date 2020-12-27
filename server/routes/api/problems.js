cff787c821b1cdbfe6a327437e71552b26b2f7be
Harry Chen <harrychen0314@gmail.com>
2020-12-26 21:06:38 -0800

    add post new problem handler

--- server/routes/api/problems.js
+++ server/routes/api/problems.js
@@ -4,12 +4,21 @@ const mongodb = require('mongodb');
 const router = express.Router();
 
 async function loadProblemsCollection() {
-    const client = await mongodb.MongoClient.connect('mongodb+srv://harry:3g2ZSNMaAGe7NDu6@fbla21-dev.lrnik.mongodb.net/problems?retryWrites=true&w=majority', {
+    const client = await mongodb.MongoClient.connect('mongodb+srv://harry:3g2ZSNMaAGe7NDu6@fbla21-dev.lrnik.mongodb.net/server?retryWrites=true&w=majority', {
         useNewUrlParser: true,
         useUnifiedTopology: true
     });
-    return client.db('problems').collection('inventory');
+    return client.db('server').collection('problems');
 }
+
+async function loadAnswersCollection() {
+    const client = await mongodb.MongoClient.connect('mongodb+srv://harry:3g2ZSNMaAGe7NDu6@fbla21-dev.lrnik.mongodb.net/server?retryWrites=true&w=majority', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    });
+    return client.db('server').collection('answers');
+}
+
 
 function getRandomInteger(min, max) {
     return min + Math.floor(Math.random() * (max - min));
@@ -23,7 +32,7 @@ router.get('/', async (req, res) => {
     const existedIndexs = new Set();
     let count = 0;
     if (req.body.count > allProblemsCount) {
-        res.send('Wrong Problem Numbers!');
+        res.send('Error: Wrong Problem Counts!');
     } else {
         while (count < req.body.count) {
             const index = getRandomInteger(0, allProblemsCount);
@@ -35,6 +44,21 @@ router.get('/', async (req, res) => {
         }
         res.send(userProblems);
     }
+});
+
+//Under construction: Not yet protected with authentication
+router.post('/', async (req, res) => {
+    const problemsCollection = await loadProblemsCollection();
+    const answersCollection = await loadAnswersCollection();
+    const newProblem = req.body;
+    delete newProblem.answer;
+    const newAnswer = {
+        problemId: req.body.problemId,
+        answer: req.body.answer
+    };
+    await problemsCollection.insertOne(newProblem);
+    await answersCollection.insertOne(newAnswer);
+    res.send('Success!');
 });
 
 module.exports = router;
