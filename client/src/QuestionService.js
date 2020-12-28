86e1642e572f6933ce97ebb0f1b6bd5b5e2e9754
Harry Chen <harrychen0314@gmail.com>
2020-12-28 12:36:22 -0800

    add gradeQuestions service

--- client/src/QuestionService.js
+++ client/src/QuestionService.js
@@ -1,16 +1,25 @@
 import axios from 'axios';
 
-const url = 'http://localhost:5000/api/questions';
+const url = 'http://localhost:5000/api/questions/';
 
 class QuestionService {
-    static getQuestions(count){
-        return new Promise((resolve,reject)=>{
+    static getQuestions(count) {
+        return new Promise((resolve, reject) => {
             axios.get(url, {
                 params: {
                     'count': count
                 }
             }).then(data => resolve(data))
-            .catch(err => reject(err));
+                .catch(err => reject(err));
+        });
+    }
+
+    static gradeQuestions(answers) {
+        return new Promise((resolve, reject) => {
+            axios.post(`${url}grade`, {
+                 answers: answers }
+            ).then(score => resolve(score))
+                .catch(err => reject(err));
         });
     }
 }
