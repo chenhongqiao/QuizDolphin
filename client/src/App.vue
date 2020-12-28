4ff35659735e3a615aa3ebbbf09ff38171cd9bb2
Harry Chen <harrychen0314@gmail.com>
2020-12-28 12:36:47 -0800

    update App component

--- client/src/App.vue
+++ client/src/App.vue
@@ -7,14 +7,21 @@
       <v-container v-if="!quizStarted">
         <v-row>
           <v-col>
-          <v-card-actions class="justify-center">
-            <v-btn @click="startQuiz">Start Quiz!</v-btn>
-          </v-card-actions>
+            <v-card-actions class="justify-center">
+              <v-btn @click="startQuiz">Start Quiz!</v-btn>
+            </v-card-actions>
           </v-col>
         </v-row>
       </v-container>
-      <v-container v-if="quizStarted">
-        <QuizComponent v-bind:quizData="quizData" />
+      <v-container v-if="quizStarted && !quizGraded">
+        <QuizComponent
+          :quizData="quizData"
+          :quizAnswers.sync="quizAnswers"
+          v-on:quizDone="gradeQuiz"
+        />
+      </v-container>
+      <v-container v-if="quizGraded">
+        <div>you got {{ quizScore }}</div>
       </v-container>
     </v-main>
   </v-app>
@@ -29,15 +36,28 @@ export default {
   components: {
     QuizComponent,
   },
-  methods: {
-    async startQuiz() {
-      this.quizStarted = true;
-      this.quizData = (await QuestionService.getQuestions(3)).data;
-    },
-  },
   data: () => ({
     quizStarted: false,
     quizData: [],
+    quizScore: 0,
+    quizAnswers: [],
+    quizGraded: false,
   }),
+  methods: {
+    async startQuiz() {
+      this.quizData = (await QuestionService.getQuestions(3)).data;
+      this.quizStarted = true;
+    },
+    async gradeQuiz() {
+      const processedAnswers = this.quizAnswers.map((value, index) => {
+        return { uuid: this.quizData[index].uuid, answer: value };
+      });
+      const rawResponse = await QuestionService.gradeQuestions(
+        processedAnswers
+      );
+      this.quizScore = rawResponse.data.score;
+      this.quizGraded = true;
+    },
+  },
 };
 </script>
