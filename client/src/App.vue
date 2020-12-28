48148b4523fd534999b90e6cb7faa53cd7d04561
Harry Chen <harrychen0314@gmail.com>
2020-12-28 11:25:42 -0800

    add QuizComponent

--- client/src/App.vue
+++ client/src/App.vue
@@ -3,25 +3,41 @@
     <v-app-bar app>
       <v-toolbar-title>FBLA Quiz System</v-toolbar-title>
     </v-app-bar>
-
     <v-main>
-      <QuizComponent />
+      <v-container v-if="!quizStarted">
+        <v-row>
+          <v-col>
+          <v-card-actions class="justify-center">
+            <v-btn @click="startQuiz">Start Quiz!</v-btn>
+          </v-card-actions>
+          </v-col>
+        </v-row>
+      </v-container>
+      <v-container v-if="quizStarted">
+        <QuizComponent v-bind:quizData="quizData" />
+      </v-container>
     </v-main>
   </v-app>
 </template>
 
 <script>
 import QuizComponent from "./components/QuizComponent";
-
+import QuestionService from "./QuestionService";
 export default {
   name: "App",
 
   components: {
     QuizComponent,
   },
-
+  methods: {
+    async startQuiz() {
+      this.quizStarted = true;
+      this.quizData = (await QuestionService.getQuestions(3)).data;
+    },
+  },
   data: () => ({
-    //
+    quizStarted: false,
+    quizData: [],
   }),
 };
 </script>
--- client/src/components/QuizComponent.vue
+++ client/src/components/QuizComponent.vue
@@ -1,13 +1,41 @@
 <template>
-  <div>FBLA Quiz System</div>
+  <v-row>
+    <v-col>
+      <div>
+        <h2>Problem #{{ currentQuestion + 1 }}</h2>
+        <div>Points: {{ quizData[currentQuestion].points }}</div>
+      </div>
+      <div>
+        <h3>Context:</h3>
+        <div class="text-center">
+          {{ quizData[currentQuestion].context }}
+        </div>
+      </div>
+      <div>
+        <h3>Options:</h3>
+        <v-radio-group v-model="answer[currentQuestion]" row>
+          <v-radio
+            v-for="option in quizData[currentQuestion].options"
+            :key="option"
+            :label="option"
+            :value="option"
+          ></v-radio>
+        </v-radio-group>
+      </div>
+      <div class="text-right">
+        <v-btn class="text-right" @click="currentQuestion++">Next</v-btn>
+      </div>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
-  export default {
-    name: 'QuizCOmponent',
-
-    data: () => ({
-      
-    }),
-  }
+export default {
+  name: "QuizComponent",
+  props: ["quizData"],
+  data: () => ({
+    currentQuestion: 0,
+    answer: [],
+  }),
+};
 </script>
