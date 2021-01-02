30bfb70961d037485ccea1aef4dee26cfc253dc9
Harry Chen <harrychen0314@gmail.com>
2021-01-02 15:02:46 -0800

    prepare to use vue router

--- client/src/App.vue
+++ client/src/App.vue
@@ -53,64 +53,11 @@
       <div v-if="!loggedIn">
         <LoginComponent
           :logged-in.sync="loggedIn"
-          @authDone="getHistory()"
+          @authDone="refresh()"
         />
       </div>
-      <div v-if="!quizStarted">
-        <div v-if="quizHistory">
-          <v-container>
-            <h3 class="text-center">
-              Previous Attempts
-            </h3>
-          </v-container>
-          <v-container>
-            <v-expansion-panels>
-              <v-expansion-panel
-                v-for="(record, index) in quizHistory"
-                :key="'r'+index+record"
-              >
-                <v-expansion-panel-header>
-                  <div>
-                    <b>{{ toLocalTime(record) }} </b>
-                    {{ record.score.toFixed(2) }} out of {{ record.totalPoints }}
-                  </div>
-                </v-expansion-panel-header>
-                <v-expansion-panel-content>
-                  <ResultComponent
-                    :quiz-result="record"
-                    :view-only="true"
-                  />
-                </v-expansion-panel-content>
-              </v-expansion-panel>
-            </v-expansion-panels>
-          </v-container>
-        </div>
-        <v-container>
-          <h3 class="text-center">
-            New Attempt
-          </h3>
-        </v-container>
-        <v-container
-          class="text-center"
-        >
-          <v-btn @click="startQuiz">
-            start!
-          </v-btn>
-        </v-container>
-      </div>
-      <div v-if="quizStarted && !quizGraded">
-        <QuizComponent
-          :quiz-data="quizData"
-          :quiz-answers.sync="quizAnswers"
-          :current-index.sync="currentIndex"
-          @quizDone="gradeQuiz"
-        />
-      </div>
-      <div v-if="quizGraded">
-        <ResultComponent
-          :quiz-result="quizResult"
-          :view-only="false"
-        />
+      <div>
+        <QuizDashComponent @notLoggedIn="loggedIn=false" />
       </div>
     </v-main>
     <v-footer>
@@ -125,55 +72,26 @@
 </template>
 
 <script>
-import { DateTime } from 'luxon';
-import QuizComponent from './components/QuizComponent.vue';
 import LoginComponent from './components/LoginComponent.vue';
-import ResultComponent from './components/ResultComponent.vue';
-import QuestionService from './QuestionService';
-import ResultService from './ResultService';
-import QuizService from './QuizService';
+import QuizDashComponent from './components/QuizDashComponent.vue';
 import UserService from './UserService';
 
 export default {
   name: 'App',
 
   components: {
-    QuizComponent,
-    ResultComponent,
     LoginComponent,
+    QuizDashComponent,
   },
   data: () => ({
-    quizStarted: false,
-    quizData: [],
-    quizResult: {},
-    quizAnswers: [],
-    quizGraded: false,
-    quizHistory: null,
     loggedIn: false,
-    currentIndex: 1,
-    progressVersion: 1,
     hasError: false,
     errorMessage: '',
   }),
-  watch: {
-    quizAnswers: {
-      handler() {
-        this.progressVersion += 1;
-        this.postProgress();
-      },
-    },
-    currentIndex: {
-      handler() {
-        this.progressVersion += 1;
-        this.postProgress();
-      },
-    },
-  },
   async beforeMount() {
     const userStatus = (await UserService.getUserStatus()).data;
     if (userStatus === 'Logged In!') {
       this.loggedIn = true;
-      this.getHistory();
     }
   },
   errorCaptured(err) {
@@ -181,63 +99,6 @@ export default {
     this.hasError = true;
   },
   methods: {
-    async startQuiz() {
-      if (this.quizData === 'Not Logged In!') {
-        this.loggedIn = false;
-      } else {
-        const previous = (await QuizService.getOngoing()).data;
-        if (!previous.question) {
-          this.quizData = (await QuestionService.getQuestions(5)).data;
-          this.quizData.forEach((question, index) => {
-            if (question.type === 'single choice' || question.type === 'short response') {
-              this.quizAnswers[index] = '';
-            } else if (question.type === 'multiple choice' || question.type === 'matching' || question.type === 'fill in the blanks') {
-              this.quizAnswers[index] = [];
-            }
-          });
-          this.postProgress();
-        } else {
-          this.quizData = previous.question;
-          const rawResponse = (await QuizService.getProgress()).data;
-          this.quizAnswers = rawResponse.attempt;
-          if (!this.quizAnswers) {
-            this.quizData.forEach((question, index) => {
-              if (question.type === 'single choice' || question.type === 'short response') {
-                this.quizAnswers[index] = '';
-              } else if (question.type === 'multiple choice' || question.type === 'matching' || question.type === 'fill in the blanks') {
-                this.quizAnswers[index] = [];
-              }
-            });
-            this.postProgress();
-          }
-          this.currentIndex = rawResponse.index;
-          this.progressVersion = rawResponse.version;
-        }
-        this.quizStarted = true;
-      }
-    },
-    async gradeQuiz() {
-      const processedAnswers = this.quizAnswers.map((value, index) => ({
-        uuid: this.quizData[index].uuid,
-        answer: value,
-      }));
-      this.quizResult = (await ResultService.gradeQuiz(
-        processedAnswers,
-      )).data;
-      if (this.quizResult === 'Not Logged In!') {
-        this.loggedIn = false;
-      } else {
-        this.quizGraded = true;
-      }
-    },
-    async getHistory() {
-      const rawResponse = (await QuizService.getQuizHistory()).data;
-      if (rawResponse === 'Not Logged In!') {
-        this.loggedIn = false;
-      } else if (rawResponse !== 'No History!') {
-        this.quizHistory = rawResponse.reverse();
-      }
-    },
     async logout() {
       const rawResponse = (await UserService.logout()).data;
       if (rawResponse === 'Success!' || rawResponse === 'Not Logged In!') {
@@ -245,16 +106,8 @@ export default {
         this.loggedIn = false;
       }
     },
-    async postProgress() {
-      const rawResponse = (await QuizService.postProgress(
-        { version: this.progressVersion, index: this.currentIndex, attempt: this.quizAnswers },
-      )).data;
-      if (rawResponse !== 'Success!') {
-        console.log('Version Too Old!');
-      }
-    },
-    toLocalTime(record) {
-      return DateTime.fromISO(record.timeStamp).setZone('America/Los_Angeles').toLocaleString(DateTime.DATETIME_MED);
+    refresh() {
+      window.location.reload();
     },
   },
 };
---
+++ client/src/components/QuizDashComponent.vue
@@ -0,0 +1,174 @@
+<template>
+  <div>
+    <div v-if="!quizStarted">
+      <div v-if="quizHistory">
+        <v-container>
+          <h3 class="text-center">
+            Previous Attempts
+          </h3>
+        </v-container>
+        <v-container>
+          <v-expansion-panels>
+            <v-expansion-panel
+              v-for="(record, index) in quizHistory"
+              :key="'r'+index+record"
+            >
+              <v-expansion-panel-header>
+                <div>
+                  <b>{{ toLocalTime(record) }} </b>
+                  {{ record.score.toFixed(2) }} out of {{ record.totalPoints }}
+                </div>
+              </v-expansion-panel-header>
+              <v-expansion-panel-content>
+                <ResultComponent
+                  :quiz-result="record"
+                  :view-only="true"
+                />
+              </v-expansion-panel-content>
+            </v-expansion-panel>
+          </v-expansion-panels>
+        </v-container>
+      </div>
+      <v-container>
+        <h3 class="text-center">
+          New Attempt
+        </h3>
+      </v-container>
+      <v-container
+        class="text-center"
+      >
+        <v-btn @click="startQuiz">
+          start!
+        </v-btn>
+      </v-container>
+    </div>
+    <div v-if="quizStarted && !quizGraded">
+      <QuizComponent
+        :quiz-data="quizData"
+        :quiz-answers.sync="quizAnswers"
+        :current-index.sync="currentIndex"
+        @quizDone="gradeQuiz"
+      />
+    </div>
+    <div v-if="quizGraded">
+      <ResultComponent
+        :quiz-result="quizResult"
+        :view-only="false"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import { DateTime } from 'luxon';
+import QuizComponent from './QuizComponent.vue';
+import ResultComponent from './ResultComponent.vue';
+import QuestionService from '../QuestionService';
+import ResultService from '../ResultService';
+import QuizService from '../QuizService';
+
+export default {
+  name: 'QuizDashComponent',
+  components: {
+    QuizComponent,
+    ResultComponent,
+  },
+  data: () => ({
+    quizStarted: false,
+    quizData: [],
+    quizResult: {},
+    quizAnswers: [],
+    quizGraded: false,
+    quizHistory: null,
+    currentIndex: 1,
+    progressVersion: 1,
+  }),
+  watch: {
+    quizAnswers: {
+      handler() {
+        this.progressVersion += 1;
+        this.postProgress();
+      },
+    },
+    currentIndex: {
+      handler() {
+        this.progressVersion += 1;
+        this.postProgress();
+      },
+    },
+  },
+  async beforeMount() {
+    this.getHistory();
+  },
+  methods: {
+    async startQuiz() {
+      if (this.quizData === 'Not Logged In!') {
+        this.$emit('notLoggedIn');
+      } else {
+        const previous = (await QuizService.getOngoing()).data;
+        if (!previous.question) {
+          this.quizData = (await QuestionService.getQuestions(5)).data;
+          this.quizData.forEach((question, index) => {
+            if (question.type === 'single choice' || question.type === 'short response') {
+              this.quizAnswers[index] = '';
+            } else if (question.type === 'multiple choice' || question.type === 'matching' || question.type === 'fill in the blanks') {
+              this.quizAnswers[index] = [];
+            }
+          });
+          this.postProgress();
+        } else {
+          this.quizData = previous.question;
+          const rawResponse = (await QuizService.getProgress()).data;
+          this.quizAnswers = rawResponse.attempt;
+          if (!this.quizAnswers) {
+            this.quizData.forEach((question, index) => {
+              if (question.type === 'single choice' || question.type === 'short response') {
+                this.quizAnswers[index] = '';
+              } else if (question.type === 'multiple choice' || question.type === 'matching' || question.type === 'fill in the blanks') {
+                this.quizAnswers[index] = [];
+              }
+            });
+            this.postProgress();
+          }
+          this.currentIndex = rawResponse.index;
+          this.progressVersion = rawResponse.version;
+        }
+        this.quizStarted = true;
+      }
+    },
+    async gradeQuiz() {
+      const processedAnswers = this.quizAnswers.map((value, index) => ({
+        uuid: this.quizData[index].uuid,
+        answer: value,
+      }));
+      this.quizResult = (await ResultService.gradeQuiz(
+        processedAnswers,
+      )).data;
+      if (this.quizResult === 'Not Logged In!') {
+        this.$emit('notLoggedIn');
+      } else {
+        this.quizGraded = true;
+      }
+    },
+    async getHistory() {
+      const rawResponse = (await QuizService.getQuizHistory()).data;
+      if (rawResponse === 'Not Logged In!') {
+        this.$emit('notLoggedIn');
+      } else if (rawResponse !== 'No History!') {
+        this.quizHistory = rawResponse.reverse();
+      }
+    },
+    async postProgress() {
+      const rawResponse = (await QuizService.postProgress(
+        { version: this.progressVersion, index: this.currentIndex, attempt: this.quizAnswers },
+      )).data;
+      if (rawResponse !== 'Success!') {
+        console.log('Version Too Old!');
+      }
+    },
+    toLocalTime(record) {
+      return DateTime.fromISO(record.timeStamp).setZone('America/Los_Angeles').toLocaleString(DateTime.DATETIME_MED);
+    },
+  },
+};
+</script>
