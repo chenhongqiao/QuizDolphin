45a113c24877b8d33104319bf2983f23d38396e8
Harry Chen <harrychen0314@gmail.com>
2021-01-02 11:12:06 -0800

    update backend ongoing handler

--- server/routes/api/quiz.js
+++ server/routes/api/quiz.js
@@ -25,6 +25,25 @@ router.get('/history', async (req, res, next) => {
   }
 });
 
+router.get('/ongoingquestion', async (req, res, next) => {
+  try {
+    if (!req.session.loggedin) {
+      res.send('Not Logged In!');
+      return;
+    }
+    const onGoingCollection = await dbService.loadCollection('ongoing');
+    const onGoingQuestion = await onGoingCollection.findOne({ email: req.session.email });
+    if (onGoingQuestion) {
+      res.send(onGoingQuestion);
+    } else {
+      res.send('No Ongoing Quiz!');
+    }
+  } catch (err) {
+    res.status(500).send('Internal Error!');
+    next(err);
+  }
+});
+
 router.get('/ongoing', async (req, res, next) => {
   try {
     if (!req.session.loggedin) {
@@ -33,7 +52,7 @@ router.get('/ongoing', async (req, res, next) => {
     }
     const redis = redisService.loadDatabase(0);
     const redisGet = promisify(redis.get).bind(redis);
-    res.send(await redisGet(req.session.email));
+    res.send(JSON.parse((await redisGet(req.session.email))));
   } catch (err) {
     res.status(500).send('Internal Error!');
     next(err);
@@ -50,8 +69,8 @@ router.post('/ongoing', async (req, res, next) => {
     const redisGet = promisify(redis.get).bind(redis);
     const redisSet = promisify(redis.set).bind(redis);
     const current = JSON.parse((await redisGet(req.session.email)));
-    if (!current.version || current.version < res.body.data.version) {
-      await redisSet(res.session.email, JSON.stringify(res.body.data.attempt));
+    if (!current || current.version < req.body.data.version) {
+      await redisSet(req.session.email, JSON.stringify(req.body.data));
       res.send('Success!');
     } else {
       res.send('Refuse to overwrite newer version with older one!');
