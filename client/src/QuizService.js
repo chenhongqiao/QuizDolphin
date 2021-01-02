268e93c7828ba55fb5b103d58341265719512532
Harry Chen <harrychen0314@gmail.com>
2021-01-02 11:11:05 -0800

    add get ongoing to backend

--- client/src/QuizService.js
+++ client/src/QuizService.js
@@ -13,6 +13,34 @@ class QuizService {
         .catch((err) => reject(err));
     });
   }
+
+  static getOngoing() {
+    return new Promise((resolve, reject) => {
+      axios.get(`${url}ongoingquestion`, {
+        withCredentials: true,
+      }).then((data) => resolve(data))
+        .catch((err) => reject(err));
+    });
+  }
+
+  static postProgress(quizProgress) {
+    return new Promise((resolve, reject) => {
+      axios.post(`${url}ongoing`, {
+        withCredentials: true,
+        data: quizProgress,
+      }).then((data) => resolve(data))
+        .catch((err) => reject(err));
+    });
+  }
+
+  static getProgress() {
+    return new Promise((resolve, reject) => {
+      axios.get(`${url}ongoing`, {
+        withCredentials: true,
+      }).then((data) => resolve(data))
+        .catch((err) => reject(err));
+    });
+  }
 }
 
 export default QuizService;
