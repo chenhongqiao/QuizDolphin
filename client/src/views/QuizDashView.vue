c04b33048a9915c475207ec62134a299906596a2
Harry Chen <harrychen0314@gmail.com>
2021-01-03 20:51:58 -0800

    add show performance graph tab

--- client/src/views/QuizDashView.vue
+++ client/src/views/QuizDashView.vue
@@ -1,34 +1,6 @@
 <template>
   <div>
     <div v-if="!quizStarted">
-      <div v-if="quizHistory">
-        <v-container>
-          <h3 class="text-center">
-            Previous Attempts
-          </h3>
-        </v-container>
-        <v-container>
-          <v-expansion-panels>
-            <v-expansion-panel
-              v-for="(record, index) in quizHistory"
-              :key="'r'+index+record"
-            >
-              <v-expansion-panel-header>
-                <div>
-                  <b>{{ toLocalTime(record) }} </b>
-                  {{ record.score.toFixed(2) }} out of {{ record.totalPoints }}
-                </div>
-              </v-expansion-panel-header>
-              <v-expansion-panel-content>
-                <ResultComponent
-                  :quiz-result="record"
-                  :view-only="true"
-                />
-              </v-expansion-panel-content>
-            </v-expansion-panel>
-          </v-expansion-panels>
-        </v-container>
-      </div>
       <v-container>
         <h3 class="text-center">
           New Attempt
@@ -37,9 +9,62 @@
       <v-container
         class="text-center"
       >
-        <v-btn @click="startNewQuiz">
+        <v-btn
+          :disabled="actionDisabled"
+          @click="startNewQuiz"
+        >
           start!
         </v-btn>
+      </v-container>
+      <v-container>
+        <v-tabs>
+          <v-tab> Records </v-tab>
+          <v-tab> Graphs </v-tab>
+
+          <v-tab-item>
+            <div v-if="quizHistory">
+              <v-container>
+                <h3 class="text-center">
+                  Previous Attempts
+                </h3>
+              </v-container>
+              <v-container>
+                <v-expansion-panels>
+                  <v-expansion-panel
+                    v-for="(record, index) in quizHistory"
+                    :key="'r'+index+record"
+                  >
+                    <v-expansion-panel-header>
+                      <div>
+                        <b>{{ toLocalTime(record) }} </b>
+                        {{ record.score.toFixed(2) }} out of {{ record.totalPoints }}
+                      </div>
+                    </v-expansion-panel-header>
+                    <v-expansion-panel-content>
+                      <ResultComponent
+                        :quiz-result="record"
+                        :view-only="true"
+                      />
+                    </v-expansion-panel-content>
+                  </v-expansion-panel>
+                </v-expansion-panels>
+              </v-container>
+            </div>
+          </v-tab-item>
+
+          <v-tab-item>
+            <v-container>
+              <h3 class="text-center">
+                Performance History
+              </h3>
+              <LineChartComponent
+                :chart-data="historyChartData"
+                :options="{maintainAspectRatio: false}"
+                :style="'height=200px;'"
+              />
+            </v-container>
+          </v-tab-item>
+        </v-tabs>
       </v-container>
     </div>
     <div v-if="quizStarted && quizLoaded && !quizGraded">
@@ -66,12 +91,14 @@ import ResultComponent from '../components/ResultComponent.vue';
 import QuestionService from '../QuestionService';
 import ResultService from '../ResultService';
 import QuizService from '../QuizService';
+import LineChartComponent from '../components/LineChartComponent.vue';
 
 export default {
   name: 'QuizDashView',
   components: {
     QuizComponent,
     ResultComponent,
+    LineChartComponent,
   },
   data: () => ({
     quizStarted: true,
@@ -84,6 +111,17 @@ export default {
     progressVersion: 1,
     quizId: 1,
     quizLoaded: false,
+    actionDisabled: false,
+    historyChartData: {
+      labels: [],
+      datasets: [
+        {
+          label: 'Quiz Score (percentage)',
+          backgroundColor: '#1565c0',
+          data: [],
+        },
+      ],
+    },
   }),
   watch: {
     quizAnswers: {
@@ -102,7 +140,6 @@ export default {
   async beforeMount() {
     this.quizId = this.$route.params.id;
     const previous = (await QuizService.getOngoing(this.quizId)).data;
-    console.log(previous);
     if (previous.question) {
       this.quizData = previous.question;
       const rawResponse = (await QuizService.getProgress(this.quizId)).data;
@@ -116,7 +153,6 @@ export default {
           }
         });
       }
-      console.log(rawResponse.index);
       this.currentIndex = rawResponse.index;
       this.progressVersion = rawResponse.version;
       this.quizLoaded = true;
@@ -127,8 +163,8 @@ export default {
   },
   methods: {
     async startNewQuiz() {
+      this.actionDisabled = true;
       this.quizData = (await QuestionService.getQuestions(5, this.quizId)).data;
-      console.log(this.quizData);
       this.quizData.forEach((question, index) => {
         if (question.type === 'single choice' || question.type === 'short response') {
           this.quizAnswers[index] = '';
@@ -155,12 +191,28 @@ export default {
         this.quizGraded = true;
       }
     },
+    getOrdinal(number) {
+      if (number % 10 === 1 && number % 100 !== 11) {
+        return `${number}st`;
+      }
+      if (number % 10 === 2 && number % 100 !== 12) {
+        return `${number}nd`;
+      }
+      if (number % 10 === 3 && number % 100 !== 13) {
+        return `${number}rd`;
+      }
+      return `${number}th`;
+    },
     async getHistory() {
       const rawResponse = (await QuizService.getQuizHistory(this.quizId)).data;
       if (rawResponse === 'Not Logged In!') {
         this.$store.state.loggedIn = false;
         this.$router.push('/login');
       } else if (rawResponse !== 'No History!') {
+        rawResponse.forEach((value, index) => {
+          this.historyChartData.labels.push(this.getOrdinal(index + 1));
+          this.historyChartData.datasets[0].data.push((value.score / value.totalPoints) * 100);
+        });
         this.quizHistory = rawResponse.reverse();
       }
     },
