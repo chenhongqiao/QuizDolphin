78b6372ee9a25aae4f2c5d6f82abae177e822ffe
Harry Chen <harrychen0314@gmail.com>
2021-01-10 18:40:08 -0800

    update redisService

--- server/modules/redisService.js
+++ server/modules/redisService.js
@@ -1,4 +1,14 @@
-module.exports = require('redis').createClient({
+const redis = require('redis');
+const { promisify } = require('util');
+
+const client = redis.createClient({
   port: process.env.REDISPORT,
   host: process.env.REDISHOST,
 });
+const get = promisify(client.get).bind(client);
+const set = promisify(client.set).bind(client);
+const del = promisify(client.del).bind(client);
+
+module.exports = {
+  get, set, del, client,
+};
