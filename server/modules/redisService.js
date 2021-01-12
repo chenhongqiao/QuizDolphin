4be28f1ed1e9c515496849891aa64060bd05a745
Harry Chen <harrychen0314@gmail.com>
2021-01-12 10:25:40 -0800

    add setnx to redisService

--- server/modules/redisService.js
+++ server/modules/redisService.js
@@ -7,8 +7,9 @@ const client = redis.createClient({
 });
 const get = promisify(client.get).bind(client);
 const set = promisify(client.set).bind(client);
+const setnx = promisify(client.setnx).bind(client);
 const del = promisify(client.del).bind(client);
 
 module.exports = {
-  get, set, del, client,
+  get, set, setnx, del, client,
 };
