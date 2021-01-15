2d8740102d178975b34437bc3daf2b86568c373d
Harry Chen <harrychen0314@gmail.com>
2021-01-15 13:46:39 -0800

    use mongodb index to increase performance

--- server/modules/dbService.js
+++ server/modules/dbService.js
@@ -6,6 +6,19 @@ const clientPromise = mongodb.MongoClient.connect(process.env.MONGOURL, {
 });
 async function loadCollection(collectionName) {
   const client = await clientPromise;
+  if (collectionName === 'users') {
+    client.db('server').collection(collectionName).createIndex({ email: 1 }, { unique: true });
+  } else if (collectionName === 'quizzes') {
+    client.db('server').collection(collectionName).createIndex({ quizId: 1 }, { unique: true });
+  } else {
+    const type = collectionName.split('-');
+    if (type[1] === 'answers' || type[1] === 'questions') {
+      client.db('server').collection(collectionName).createIndex({ questionId: 1 }, { unique: true });
+    } else if (type[1] === 'attempts' || type[1] === 'results') {
+      client.db('server').collection(collectionName).createIndex({ attemptId: 1 }, { unique: true });
+      client.db('server').collection(collectionName).createIndex({ email: 1 });
+    }
+  }
   return client.db('server').collection(collectionName);
 }
 
