8e02ac4e3839fad98a4874a63dad73484b848989
Harry Chen <harrychen0314@gmail.com>
2021-01-26 21:25:22 -0800

    add exception handler for attempt api and service

--- server/api/attempt.js
+++ server/api/attempt.js
@@ -7,7 +7,7 @@ const gradingService = require('../services/grading');
 
 router.get('/:attemptId/progress', async (req, res, next) => {
   try {
-    if (!req.session.loggedin) {
+    if (!req.session.loggedin || !req.session.email) {
       res.status(401).send('Not Logged In!');
       return;
     }
@@ -16,7 +16,15 @@ router.get('/:attemptId/progress', async (req, res, next) => {
       res.status(400).send('Missing Parameter!');
       return;
     }
-    res.send(await attemptService.getProgress(attemptId));
+    const response = await attemptService.getProgress(attemptId);
+    if (!response.success) {
+      if (response.message === 'No Matching Progress!') {
+        res.status(404).send('No Matching Progress!');
+        return;
+      }
+    } else {
+      res.send(response.data);
+    }
   } catch (err) {
     next(err);
   }
@@ -24,7 +32,7 @@ router.get('/:attemptId/progress', async (req, res, next) => {
 
 router.post('/:attemptId/progress', async (req, res, next) => {
   try {
-    if (!req.session.loggedin) {
+    if (!req.session.loggedin || !req.session.email) {
       res.status(401).send('Not Logged In!');
       return;
     }
@@ -32,6 +40,23 @@ router.post('/:attemptId/progress', async (req, res, next) => {
     if (!attemptId) {
       res.status(400).send('Missing');
       return;
+    }
+    const response = await attemptService.postProgress(attemptId, req.body.data);
+    if (!response.success) {
+      if (response.message === 'No Matching Progress!') {
+        res.status(404).send('No Matching Progress!');
+        return;
+      }
+      if (response.message === 'Refuse To Overwrite!') {
+        res.status(409).send('Refuse To Overwrite!');
+        return;
+      }
+      if (response.message === 'Quiz Ended!') {
+        res.status(410).send('Quiz Ended!');
+        return;
+      }
+    } else {
+      res.send(response.data);
     }
     res.send(await attemptService.postProgress(attemptId, req.body.data));
   } catch (err) {
@@ -50,7 +75,16 @@ router.get('/:attemptId', async (req, res, next) => {
       res.status(400).send('Missing Parameters!');
       return;
     }
-    res.send(await attemptService.getAttemptData(attemptId));
+    const response = await attemptService.getAttemptData(attemptId);
+    if (!response.success) {
+      if (response.message === 'No Matching Attempt!') {
+        res.status(404).send('No Matching Attempt!');
+        return;
+      }
+    } else {
+      res.send(response.data);
+    }
+    res.send();
   } catch (err) {
     next(err);
   }
--- server/services/attempt.js
+++ server/services/attempt.js
@@ -2,33 +2,49 @@ const redis = require('../databases/redis');
 const mongodb = require('../databases/mongodb');
 
 async function getProgress(attemptId) {
-  const progress = JSON.parse(await redis.get(`progress:${attemptId}`));
-  return progress;
+  const progressString = await redis.get(`progress:${attemptId}`);
+  if (!progressString) {
+    return { success: false, message: 'No Matching Progress!' };
+  }
+  const progress = JSON.parse(progressString);
+  return { success: true, data: progress };
 }
 
 async function postProgress(attemptId, progress) {
   const endTime = await redis.get(`endTime:${attemptId}`);
   if (!endTime) {
     const attemptsCollection = await mongodb.loadCollection('attempts');
-    const QuizData = await attemptsCollection.findOne({ attemptId });
-    await redis.setnx(`endTime:${QuizData.attemptId}`, JSON.stringify(QuizData.endTime));
+    const quizDataCursor = await attemptsCollection.find({ attemptId }, { endTime: 1, _id: 0 });
+    if (!await quizDataCursor.count()) {
+      return { success: false, message: 'No Matching Progress!' };
+    }
+    const quizData = quizDataCursor.toArray()[0];
+    if (Math.floor(Date.now() / 1000) <= quizData.endTime) {
+      await redis.setnx(`endTime:${quizData.attemptId}`, JSON.stringify(quizData.endTime));
+    } else {
+      return { success: false, message: 'Quiz Ended!' };
+    }
   }
   if (Math.floor(Date.now() / 1000) <= endTime) {
     const currentProgress = JSON.parse((await redis.get(`progress:${attemptId}`)));
     if (!currentProgress || currentProgress.version < progress.version) {
       await redis.set(`progress:${attemptId}`, JSON.stringify(progress));
-      return 'Success!';
+      return { success: true };
     }
-    return 'Refuse to overwrite!';
+    return { success: false, message: 'Refuse To Overwrite!' };
   }
-  return 'Quiz Ended!';
+  return { success: false, message: 'Quiz Ended!' };
 }
 
 async function getAttemptData(attemptId) {
   const attemptsCollection = await mongodb.loadCollection('attempts');
-  const quizData = await attemptsCollection.findOne({ attemptId });
+  const quizDataCursor = await attemptsCollection.find({ attemptId }, { _id: 0 });
+  if (!await quizDataCursor.count()) {
+    return { success: false, message: 'No Matching Attempt!' };
+  }
+  const quizData = quizDataCursor.toArray()[0];
   delete quizData.answers;
-  return quizData;
+  return { success: true, data: quizData };
 }
 
 module.exports = { getProgress, postProgress, getAttemptData };
