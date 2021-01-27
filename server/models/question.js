6089ea2dd1b538ffe6fa536c35c4b257882f638e
Harry Chen <harrychen0314@gmail.com>
2021-01-26 21:44:46 -0800

    validate question in constructor

--- server/models/question.js
+++ server/models/question.js
@@ -1,16 +1,22 @@
+const validate = require('../utils/validate');
+
 function Question(question, questionId) {
-  this.questionId = questionId;
-  this.quizId = question.quizId;
-  this.context = question.context;
-  this.points = question.points;
-  this.type = question.type;
-  this.answer = question.answer;
-  if (question.type === 'single choice' || question.type === 'multiple choice' || question.type === 'fill in the blanks') {
-    this.options = question.options;
-  }
-  if (question.type === 'matching') {
-    this.rightcol = question.rightcol;
-    this.leftcol = question.leftcol;
+  if (validate.validateQuestion(question)) {
+    this.questionId = questionId;
+    this.quizId = question.quizId;
+    this.context = question.context;
+    this.points = question.points;
+    this.type = question.type;
+    this.answer = question.answer;
+    if (question.type === 'single choice' || question.type === 'multiple choice' || question.type === 'fill in the blanks') {
+      this.options = question.options;
+    }
+    if (question.type === 'matching') {
+      this.rightcol = question.rightcol;
+      this.leftcol = question.leftcol;
+    }
+  } else {
+    this.invalid = true;
   }
 }
 
