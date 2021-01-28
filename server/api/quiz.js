338e436171dfae3576ca18d606fc946aa8397e9d
Harry Chen <harrychen0314@gmail.com>
2021-01-27 18:13:28 -0800

    add error handling to quiz api and service

--- server/api/quiz.js
+++ server/api/quiz.js
@@ -4,6 +4,28 @@ const router = express.Router();
 
 const quizService = require('../services/quiz');
 
+router.get('/:quizId/ongoing', async (req, res, next) => {
+  try {
+    if (!req.session.loggedin || !req.session.email) {
+      res.status(401).send('Not Logged In!');
+      return;
+    }
+    const { quizId } = req.params;
+    if (!quizId) {
+      res.status(400).send('Missing Query!');
+      return;
+    }
+    const response = await quizService.getOngoingId(req.session.email, quizId);
+    if (!response.success) {
+      throw Error('Unexpected Service Response!');
+    } else {
+      res.send(response.data);
+    }
+  } catch (err) {
+    next(err);
+  }
+});
+
 router.get('/:quizId/attempt', async (req, res, next) => {
   try {
     if (!req.session.loggedin || !req.session.email) {
@@ -15,13 +37,19 @@ router.get('/:quizId/attempt', async (req, res, next) => {
       res.status(400).send('Missing Query!');
       return;
     }
-    const attemptId = await quizService.quizAttemptId(req.session.email, quizId);
-    if (attemptId) {
-      res.send(attemptId);
-    } else if (req.query.newQuiz === 'true') {
-      res.send(await quizService.newAttempt(quizId, req.session.email));
+    const response = await quizService.newAttempt(quizId, req.session.email);
+    if (!response.success) {
+      if (response.message === 'No Simultaneous Attempts Allowed!') {
+        res.status(409).send('No Simultaneous Attempts Allowed!');
+        return;
+      }
+      if (response.message === 'No Matching Quiz!') {
+        res.status(404).send('No Matching Quiz!');
+        return;
+      }
+      throw Error('Unexpected Service Response!');
     } else {
-      res.send('No Ongoing Attempt!');
+      res.send(response.data);
     }
   } catch (err) {
     next(err);
@@ -40,11 +68,15 @@ router.get('/:quizId/history', async (req, res, next) => {
       return;
     }
 
-    const history = await quizService.quizResults(req.session.email, quizId);
-    if (!Array.isArray(history) || history.length === 0) {
-      res.send('No History!');
+    const response = await quizService.getHistoryId(req.session.email, quizId);
+    if (!response.success) {
+      if (response.message === 'No Matching Quiz!') {
+        res.status(404).send('No Matching Quiz!');
+        return;
+      }
+      throw Error('Unexpected Service Response!');
     } else {
-      res.send(history);
+      res.send(response.data);
     }
   } catch (err) {
     next(err);
@@ -57,7 +89,12 @@ router.get('/list', async (req, res, next) => {
       res.status(401).send('Not Logged In!');
       return;
     }
-    res.send(await quizService.quizList());
+    const response = await quizService.getQuizList(req.session.email);
+    if (!response.success) {
+      throw Error('Unexpected Service Response!');
+    } else {
+      res.send(response.data);
+    }
   } catch (err) {
     next(err);
   }
@@ -72,7 +109,16 @@ router.get('/:quizId/questions', async (req, res, next) => {
       return;
     }
     const { quizId } = req.params;
-    res.send(await quizService.getQuestions(quizId));
+    const response = await quizService.getQuestions(quizId);
+    if (!response.success) {
+      if (response.message === 'No Matching Quiz!') {
+        res.status(404).send('No Matching Quiz!');
+        return;
+      }
+      throw Error('Unexpected Service Response!');
+    } else {
+      res.send(response.data);
+    }
   } catch (err) {
     next(err);
   }
@@ -84,8 +130,16 @@ router.post('/', async (req, res, next) => {
       res.status(403).send('Admin Privileges Are Required!');
       return;
     }
-    await quizService.newQuiz(req.body.data);
-    res.send('Success!');
+    const response = await quizService.newQuiz(req.body.data);
+    if (!response.success) {
+      if (response.message === 'Incorrect Quiz Syntax!') {
+        res.status(400).send('Incorrect Quiz Syntax!');
+        return;
+      }
+      throw Error('Unexpected Service Response!');
+    } else {
+      res.send(response.data);
+    }
   } catch (err) {
     next(err);
   }
@@ -98,12 +152,16 @@ router.delete('/:quizId', async (req, res, next) => {
       return;
     }
     const { quizId } = req.params;
-    const status = await quizService.deleteQuiz(quizId);
-    if (!status.matchedCount) {
-      res.status(404).send('No Matching Quiz!');
-      return;
+    const response = await quizService.deleteQuiz(quizId);
+    if (!response.success) {
+      if (response.message === 'No Matching Quiz!') {
+        res.status(404).send('No Matching Quiz!');
+        return;
+      }
+      throw Error('Unexpected Service Response!');
+    } else {
+      res.send('Success!');
     }
-    res.send('Success!');
   } catch (err) {
     next(err);
   }
@@ -116,12 +174,16 @@ router.put('/:quizId', async (req, res, next) => {
       return;
     }
     const { quizId } = req.params;
-    const status = await quizService.updateQuiz(quizId, req.body.data);
-    if (!status.matchedCount) {
-      res.status(404).send('No Matching Quiz!');
-      return;
+    const response = await quizService.updateQuiz(quizId, req.body.data);
+    if (!response.success) {
+      if (response.message === 'No Matching Quiz!') {
+        res.status(404).send('No Matching Quiz!');
+        return;
+      }
+      throw Error('Unexpected Service Response!');
+    } else {
+      res.send('Success!');
     }
-    res.send('Success!');
   } catch (err) {
     next(err);
   }
--- server/services/quiz.js
+++ server/services/quiz.js
@@ -4,103 +4,144 @@ const random = require('../utils/random');
 const nanoid = require('../utils/nanoid');
 const quizModel = require('../models/quiz');
 
-async function newAttempt(quizId, email) {
-  const questionsCollection = await mongodb.loadCollection('questions');
-  const quizCollection = await mongodb.loadCollection('quizzes');
-  const questions = await questionsCollection.find({ quizId }).toArray();
-  const quizInfo = await quizCollection.findOne({ quizId });
-  const { questionCount } = quizInfo;
-  const selectedQuestions = [];
-  const selectedAnswers = [];
-  const selectedIndexes = new Set();
-  while (selectedQuestions.length < questionCount) {
-    // console.log(selectedQuestions.length);
-    const index = random.integer(0, questions.length);
-    if (!selectedIndexes.has(index)) {
-      const { answer } = questions[index];
-      selectedAnswers.push(answer);
-      delete questions[index].answer;
-      selectedQuestions.push(questions[index]);
-      selectedIndexes.add(index);
+class QuizService {
+  static async newAttempt(quizId, email) {
+    const questionsCollection = await mongodb.loadCollection('questions');
+    if (this.getOngoingId(email, quizId).data) {
+      return { success: false, message: 'No Simultaneous Attempts Allowed!' };
     }
-  }
-  const resultsCollection = await mongodb.loadCollection('results');
-  const attemptsCollection = await mongodb.loadCollection('attempts');
-  let attemptId = nanoid.charId();
-  // eslint-disable-next-line no-await-in-loop
-  while (await resultsCollection.findOne({ attemptId })
+    const quizInfoRes = await this.getQuizInfo(quizId);
+    if (!quizInfoRes.success) {
+      return { success: false, message: 'No Matching Quiz!' };
+    }
+    const quizInfo = quizInfoRes.data;
+    const questions = await questionsCollection.find({ quizId }).toArray();
+    const { questionCount } = quizInfo;
+    const selectedQuestions = [];
+    const selectedAnswers = [];
+    const selectedIndexes = new Set();
+    while (selectedQuestions.length < questionCount) {
+    // console.log(selectedQuestions.length);
+      const index = random.integer(0, questions.length);
+      if (!selectedIndexes.has(index)) {
+        const { answer } = questions[index];
+        selectedAnswers.push(answer);
+        delete questions[index].answer;
+        selectedQuestions.push(questions[index]);
+        selectedIndexes.add(index);
+      }
+    }
+    const resultsCollection = await mongodb.loadCollection('results');
+    const attemptsCollection = await mongodb.loadCollection('attempts');
+    let attemptId = nanoid.charId();
+    // eslint-disable-next-line no-await-in-loop
+    while (await resultsCollection.findOne({ attemptId })
       // eslint-disable-next-line no-await-in-loop
       || await attemptsCollection.findOne({ attemptId })) {
-    attemptId = nanoid.charId();
-  }
-  const responses = [];
-  selectedQuestions.forEach((question, index) => {
-    if (question.type === 'single choice' || question.type === 'short response') {
-      responses[index] = '';
-    } else if (question.type === 'multiple choice' || question.type === 'matching' || question.type === 'fill in the blanks') {
-      responses[index] = [];
+      attemptId = nanoid.charId();
     }
-  });
-  const quizData = new quizModel.Data(
-    email,
-    selectedQuestions,
-    selectedAnswers,
-    quizInfo.duration,
-    attemptId,
-    quizId,
-  );
-  const initialProgress = new quizModel.Progress(1, responses, attemptId);
-  await attemptsCollection.insertOne(quizData);
-  await redis.set(`progress:${attemptId}`, JSON.stringify(initialProgress));
-  await redis.setnx(`endTime:${attemptId}`, JSON.stringify(quizData.endTime));
-  return attemptId;
-}
+    const responses = [];
+    selectedQuestions.forEach((question, index) => {
+      if (question.type === 'single choice' || question.type === 'short response') {
+        responses[index] = '';
+      } else if (question.type === 'multiple choice' || question.type === 'matching' || question.type === 'fill in the blanks') {
+        responses[index] = [];
+      }
+    });
+    const quizData = new quizModel.Data(
+      email,
+      selectedQuestions,
+      selectedAnswers,
+      quizInfo.duration,
+      attemptId,
+      quizId,
+    );
+    const initialProgress = new quizModel.Progress(1, responses, attemptId);
+    await attemptsCollection.insertOne(quizData);
+    await redis.set(`progress:${attemptId}`, JSON.stringify(initialProgress));
+    await redis.setnx(`endTime:${attemptId}`, JSON.stringify(quizData.endTime));
+    return { success: true, data: attemptId };
+  }
 
-async function quizAttemptId(email, quizId) {
-  const attemptsCollection = await mongodb.loadCollection('attempts');
-  // eslint-disable-next-line max-len
-  const attemptId = (await attemptsCollection.find({ email, quizId }).project({ attemptId: 1, _id: 0 }).toArray())[0];
-  return attemptId;
-}
+  static async getOngoingId(email, quizId) {
+    const attemptsCollection = await mongodb.loadCollection('attempts');
+    if (!(await this.getQuizInfo(quizId)).success) {
+      return { success: false, message: 'No Matching Quiz!' };
+    }
+    // eslint-disable-next-line max-len
+    const attemptId = (await attemptsCollection.find({ email, quizId }).project({ attemptId: 1, _id: 0 }).toArray())[0];
+    return { success: true, data: attemptId };
+  }
 
-async function quizResults(email, quizId) {
-  const resultsCollection = await mongodb.loadCollection('results');
-  // eslint-disable-next-line max-len
-  const results = await resultsCollection.find({
-    $query: { email, quizId },
-    $orderby: { timeStamp: 1 },
-  }).project({ attemptId: 1, _id: 0 }).toArray();
-  return results;
-}
+  static async getHistoryId(email, quizId) {
+    const resultsCollection = await mongodb.loadCollection('results');
+    if (!(await this.getQuizInfo(quizId)).success) {
+      return { success: false, message: 'No Matching Quiz!' };
+    }
+    // eslint-disable-next-line max-len
+    const results = await resultsCollection.find({
+      $query: { email, quizId },
+      $orderby: { timeStamp: 1 },
+    }).project({ attemptId: 1, _id: 0 }).toArray();
+    return { success: true, data: results };
+  }
 
-async function quizList() {
-  const quizCollection = await mongodb.loadCollection('quizzes');
-  return quizCollection.find({}).toArray();
-}
+  static async getQuizList() {
+    const quizCollection = await mongodb.loadCollection('quizzes');
+    return quizCollection.find({}).toArray();
+  }
 
-async function newQuiz(quizInfo) {
-  const quizCollection = await mongodb.loadCollection('quizzes');
-  return quizCollection.insertOne(new quizModel.Info(quizInfo, nanoid.numId));
-}
+  static async getQuizInfo(quizId) {
+    const quizCollection = await mongodb.loadCollection('quizzes');
+    const quizInfoCursor = await quizCollection.find({ quizId }).limit(1);
+    if (await quizInfoCursor.count() === 0) {
+      return { success: false, message: 'No Matching Quiz!' };
+    }
+    return { success: true, data: quizInfoCursor.toArray()[0] };
+  }
 
-async function deleteQuiz(quizId) {
-  const quizCollection = await mongodb.loadCollection('quizzes');
-  return quizCollection.deleteOne({ quizId });
-}
+  static async newQuiz(quizInfo) {
+    const quizCollection = await mongodb.loadCollection('quizzes');
+    let quizId = nanoid.numId;
+    // eslint-disable-next-line no-await-in-loop
+    while (await quizCollection.findOne({ quizId })) {
+      quizId = nanoid.numId;
+    }
+    const quiz = new quizModel.Info(quizInfo, quizId);
+    if (quiz.invalid) {
+      return { success: false, message: 'Incorrect Quiz Syntax!' };
+    }
+    quizCollection.insertOne(quiz);
+    return { success: true, data: quizId };
+  }
 
-async function updateQuiz(quizId, quizInfo) {
-  const quizCollection = await mongodb.loadCollection('quizzes');
-  return quizCollection.updateOne(
-    { quizId },
-    new quizModel.Info(quizInfo, quizId),
-  );
-}
+  static async deleteQuiz(quizId) {
+    const quizCollection = await mongodb.loadCollection('quizzes');
+    const dbRes = await quizCollection.deleteOne({ quizId });
+    if (dbRes.deletedCount === 0) {
+      return { success: false, message: 'No Matching Quiz!' };
+    }
+    return { success: true };
+  }
 
-async function getQuestions(quizId) {
-  const questionsCollection = await mongodb.loadCollection('questions');
-  return questionsCollection.find({ quizId }).toArray;
-}
+  static async updateQuiz(quizId, quizInfo) {
+    const quizCollection = await mongodb.loadCollection('quizzes');
+    const dbRes = await quizCollection.updateOne(
+      { quizId },
+      new quizModel.Info(quizInfo, quizId),
+    );
+    if (dbRes.matchedCount === 0) {
+      return { success: false, message: 'No Matching Quiz!' };
+    }
+    return { success: true };
+  }
 
-module.exports = {
-  newAttempt, quizAttemptId, quizResults, quizList, newQuiz, deleteQuiz, updateQuiz, getQuestions,
-};
+  static async getQuestions(quizId) {
+    const questionsCollection = await mongodb.loadCollection('questions');
+    if (!(await this.getQuizInfo(quizId)).success) {
+      return { success: false, message: 'No Matching Quiz!' };
+    }
+    return { success: true, data: await questionsCollection.find({ quizId }).toArray() };
+  }
+}
+module.exports = QuizService;
