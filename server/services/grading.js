3b7781d384bb1b42849fa1a6668214d8c0d9b108
Harry Chen <harrychen0314@gmail.com>
2021-01-27 18:13:49 -0800

    wrap service functions in class

--- server/services/grading.js
+++ server/services/grading.js
@@ -3,99 +3,107 @@ const redis = require('../databases/redis');
 
 const resultModel = require('../models/result');
 
-async function gradeQuiz(attemptId, email) {
-  const attemptsCollection = await mongodb.loadCollection('attempts');
-  const resultsCollection = await mongodb.loadCollection('results');
-  const attemptData = (await attemptsCollection.findOne({ attemptId }));
-  const { responses } = JSON.parse(await redis.get(`progress:${attemptId}`));
-  const results = [];
-  const { questions } = attemptData;
-  const { answers } = attemptData;
-  let totalPoints = 0;
-  let score = 0;
-  for (let index = 0; index < questions.length; index += 1) {
-    const question = questions[index];
-    const response = responses[index];
-    const answer = answers[index];
-    totalPoints += question.points;
-    // Grade single choice and short response questions
-    if (question.type === 'single choice' || question.type === 'short response') {
-      // Gain full points only if user's input match exactly with correct answer
-      if (answer === response) {
-        results.push(new resultModel.QuestionResult(
-          response, answer, question.points,
-          question.questionId, question.points,
-        ));
-        score += question.points;
-      } else {
-        results.push(new resultModel.QuestionResult(
-          response, answer, 0, question.questionId, question.points,
-        ));
-      }
+class GradingService {
+  static async gradeQuiz(attemptId, email) {
+    const attemptsCollection = await mongodb.loadCollection('attempts');
+    const resultsCollection = await mongodb.loadCollection('results');
+    const attemptDataCursor = await attemptsCollection.find({ attemptId });
+    if (await attemptDataCursor.count() === 0) {
+      return { success: false, message: 'No Matching Attempt!' };
     }
-
-    // Grade multiple choice questions
-    if (question.type === 'multiple choice') {
-      const answersSet = new Set(answer);
-      // Points are proportional to how many correction opions are chosen.
-      // Additionally, user get 0 for the entire question if incorrect options are chosen.
-      const correctCount = response.reduce((count, option) => {
-        if (answersSet.has(option)) {
-          return count + 1;
+    const attemptData = attemptDataCursor.toArray()[0];
+    if (attemptData.email !== email) {
+      return { success: false, message: 'No Privileges!' };
+    }
+    const { responses } = JSON.parse(await redis.get(`progress:${attemptId}`));
+    const results = [];
+    const { questions } = attemptData;
+    const { answers } = attemptData;
+    let totalPoints = 0;
+    let score = 0;
+    for (let index = 0; index < questions.length; index += 1) {
+      const question = questions[index];
+      const response = responses[index];
+      const answer = answers[index];
+      totalPoints += question.points;
+      // Grade single choice and short response questions
+      if (question.type === 'single choice' || question.type === 'short response') {
+      // Gain full points only if user's input match exactly with correct answer
+        if (answer === response) {
+          results.push(new resultModel.QuestionResult(
+            response, answer, question.points,
+            question.questionId, question.points,
+          ));
+          score += question.points;
+        } else {
+          results.push(new resultModel.QuestionResult(
+            response, answer, 0, question.questionId, question.points,
+          ));
         }
-        return -Infinity;
-      }, 0);
-      if (correctCount < 0) {
-        results.push(new resultModel.QuestionResult(
-          response, answer, 0, question.questionId, question.points,
-        ));
-      } else {
+      }
+
+      // Grade multiple choice questions
+      if (question.type === 'multiple choice') {
+        const answersSet = new Set(answer);
+        // Points are proportional to how many correction opions are chosen.
+        // Additionally, user get 0 for the entire question if incorrect options are chosen.
+        const correctCount = response.reduce((count, option) => {
+          if (answersSet.has(option)) {
+            return count + 1;
+          }
+          return -Infinity;
+        }, 0);
+        if (correctCount < 0) {
+          results.push(new resultModel.QuestionResult(
+            response, answer, 0, question.questionId, question.points,
+          ));
+        } else {
+          results.push(new resultModel.QuestionResult(
+            response, answer,
+            question.points * (correctCount / answersSet.size),
+            question.questionId, question.points,
+          ));
+          score += question.points * (correctCount / answersSet.size);
+        }
+      }
+
+      if (question.type === 'matching') {
+        const correctMatch = response.reduce((count, option, optionIndex) => {
+          if (option === answer[optionIndex]) {
+            return count + 1;
+          }
+          return count;
+        }, 0);
         results.push(new resultModel.QuestionResult(
           response, answer,
-          question.points * (correctCount / answersSet.size),
+          question.points * (correctMatch / answer.length),
           question.questionId, question.points,
         ));
-        score += question.points * (correctCount / answersSet.size);
+        score += question.points * (correctMatch / answer.length);
+      }
+
+      if (question.type === 'fill in the blanks') {
+        const correctMatch = response.reduce((count, option, optionIndex) => {
+          if (option === answer[optionIndex]) {
+            return count + 1;
+          }
+          return count;
+        }, 0);
+        results.push(new resultModel.QuestionResult(
+          response, answer,
+          question.points * (correctMatch / answer.length),
+          question.questionId, question.points,
+        ));
+        score += question.points * (correctMatch / answer.length);
       }
     }
-
-    if (question.type === 'matching') {
-      const correctMatch = response.reduce((count, option, optionIndex) => {
-        if (option === answer[optionIndex]) {
-          return count + 1;
-        }
-        return count;
-      }, 0);
-      results.push(new resultModel.QuestionResult(
-        response, answer,
-        question.points * (correctMatch / answer.length),
-        question.questionId, question.points,
-      ));
-      score += question.points * (correctMatch / answer.length);
-    }
-
-    if (question.type === 'fill in the blanks') {
-      const correctMatch = response.reduce((count, option, optionIndex) => {
-        if (option === answer[optionIndex]) {
-          return count + 1;
-        }
-        return count;
-      }, 0);
-      results.push(new resultModel.QuestionResult(
-        response, answer,
-        question.points * (correctMatch / answer.length),
-        question.questionId, question.points,
-      ));
-      score += question.points * (correctMatch / answer.length);
-    }
+    const quizResult = new resultModel.QuizResult(score, questions,
+      results, totalPoints, attemptId, email, attemptData.quizId);
+    await resultsCollection.insertOne(quizResult);
+    await attemptsCollection.deleteOne({ attemptId });
+    await redis.del(`progress:${attemptId}`);
+    await redis.del(`endTime:${attemptId}`);
+    return { success: true, data: attemptId };
   }
-  const quizResult = new resultModel.QuizResult(score, questions,
-    results, totalPoints, attemptId, email, attemptData.quizId);
-  await resultsCollection.insertOne(quizResult);
-  await attemptsCollection.deleteOne({ attemptId });
-  await redis.del(`progress:${attemptId}`);
-  await redis.del(`endTime:${attemptId}`);
-  return attemptId;
 }
-
-module.exports = { gradeQuiz };
+module.exports = GradingService;
--- server/services/question.js
+++ server/services/question.js
@@ -2,41 +2,42 @@ const mongodb = require('../databases/mongodb');
 const nanoid = require('../utils/nanoid');
 const questionModel = require('../models/question');
 
-async function newQuestion(question) {
-  const questionsCollection = await mongodb.loadCollection('questions');
-  let questionId = nanoid.charId();
-  // eslint-disable-next-line no-await-in-loop
-  while (await questionsCollection.findOne({ questionId })) {
-    questionId = nanoid.charId();
+class QuestionService {
+  static async newQuestion(question) {
+    const questionsCollection = await mongodb.loadCollection('questions');
+    let questionId = nanoid.charId();
+    // eslint-disable-next-line no-await-in-loop
+    while (await questionsCollection.findOne({ questionId })) {
+      questionId = nanoid.charId();
+    }
+    const questionData = new questionModel.Question(question, questionId);
+    if (questionData.invalid) {
+      return { success: false, message: 'Incorrect Question Syntax!' };
+    }
+    questionsCollection.insertOne(questionData);
+    return { success: true, data: questionId };
   }
-  const questionData = questionModel.Question(question, questionId);
-  if (questionData.invalid) {
-    return { success: false, message: 'Incorrect Question Syntax!' };
-  }
-  questionsCollection.insertOne(questionData);
-  return { success: true, data: questionId };
-}
 
-async function deleteQuestion(questionId) {
-  const questionsCollection = await mongodb.loadCollection('questions');
-  const status = await questionsCollection.deleteOne({ questionId });
-  if (status.deletedCount === 0) {
-    return { success: false, message: 'No Matching Question!' };
+  static async deleteQuestion(questionId) {
+    const questionsCollection = await mongodb.loadCollection('questions');
+    const status = await questionsCollection.deleteOne({ questionId });
+    if (status.deletedCount === 0) {
+      return { success: false, message: 'No Matching Question!' };
+    }
+    return { success: true };
   }
-  return { success: true };
-}
 
-async function updateQuestion(questionId, question) {
-  const questionsCollection = await mongodb.loadCollection('questions');
-  const questionData = questionModel.Question(question, questionId);
-  if (questionData.invalid) {
-    return { success: false, message: 'Incorrect Question Syntax!' };
+  static async updateQuestion(questionId, question) {
+    const questionsCollection = await mongodb.loadCollection('questions');
+    const questionData = new questionModel.Question(question, questionId);
+    if (questionData.invalid) {
+      return { success: false, message: 'Incorrect Question Syntax!' };
+    }
+    const status = await questionsCollection.updateOne({ questionId }, questionData);
+    if (status.matchedCount === 0) {
+      return { success: false, message: 'No Matching Question!' };
+    }
+    return { success: true };
   }
-  const status = await questionsCollection.updateOne({ questionId }, questionData);
-  if (status.matchedCount === 0) {
-    return { success: false, message: 'No Matching Question!' };
-  }
-  return { success: true };
 }
-
-module.exports = { newQuestion, deleteQuestion, updateQuestion };
+module.exports = QuestionService;
