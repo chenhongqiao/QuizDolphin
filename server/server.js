c9ac309e0165203d8ef9db98d10fd2579c878af0
Harry Chen <harrychen0314@gmail.com>
2021-01-30 17:32:04 -0800

    use loaders for server startup

--- server/server.js
+++ server/server.js
@@ -9,74 +9,77 @@ const rateLimit = require('express-rate-limit');
 const history = require('connect-history-api-fallback');
 const path = require('path');
 const redis = require('./databases/redis');
+const mongodb = require('./databases/mongodb');
+const agenda = require('./jobs/agenda');
+const questionAPI = require('./api/question');
+const attemptAPI = require('./api/attempt');
+const quizAPI = require('./api/quiz');
+const userAPI = require('./api/user');
+const resultAPI = require('./api/result');
 
-const app = express();
+async function startServer() {
+  await mongodb.connect();
+  await agenda.connect();
 
-app.use(bodyParser.json());
-app.use(cookieParser());
-app.use(cors({
-  credentials: true,
-}));
-app.use(session({
-  secret: process.env.COOKIESECRET,
-  resave: false,
-  saveUninitialized: false,
-  cookie: {
-    secure: false,
-  },
-  store: new RedisStore({ client: redis.client }),
-}));
+  const app = express();
 
-app.use('/api', rateLimit({
-  windowMs: 1000,
-  max: 4,
-  keyGenerator: (req) => {
-    if (req.session.email) {
-      return req.session.email;
+  app.use(bodyParser.json());
+  app.use(cookieParser());
+  app.use(cors({
+    credentials: true,
+  }));
+  app.use(session({
+    secret: process.env.COOKIESECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      secure: false,
+    },
+    store: new RedisStore({ client: redis.client }),
+  }));
+
+  app.use('/api', rateLimit({
+    windowMs: 1000,
+    max: 4,
+    keyGenerator: (req) => {
+      if (req.session.email) {
+        return req.session.email;
+      }
+      return req.ip;
+    },
+    message: 'Request too frequent, please try again later.',
+  }));
+
+  app.use('/api/question', questionAPI);
+
+  app.use('/api/attempt', attemptAPI);
+
+  app.use('/api/quiz', quizAPI);
+
+  app.use('/api/user', userAPI);
+
+  app.use('/api/result', resultAPI);
+
+  app.use('/api', (req, res) => {
+    res.status(404).send({ message: 'Invalid API Endpoint' });
+  });
+
+  // eslint-disable-next-line no-unused-vars
+  app.use('/api', (err, req, res, next) => {
+    if (err.status) {
+      res.status(err.status).send({ message: err.message });
+    } else {
+      res.status(500).send({ message: 'Internal Error!' });
+      // eslint-disable-next-line no-console
+      console.error(err);
     }
-    return req.ip;
-  },
-  message: 'Request too frequent, please try again later.',
-}));
+  });
 
-const question = require('./api/question');
+  app.use(history());
+  app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/api/question', question);
-
-const attempt = require('./api/attempt');
-
-app.use('/api/attempt', attempt);
-
-const quiz = require('./api/quiz');
-
-app.use('/api/quiz', quiz);
-
-const user = require('./api/user');
-
-app.use('/api/user', user);
-
-const result = require('./api/result');
-
-app.use('/api/result', result);
-
-app.use('/api', (req, res) => {
-  res.status(404).send({ message: 'Invalid API Endpoint' });
-});
-
-// eslint-disable-next-line no-unused-vars
-app.use('/api', (err, req, res, next) => {
-  if (err.status) {
-    res.status(err.status).send({ message: err.message });
-  } else {
-    res.status(500).send({ message: 'Internal Error!' });
-    // eslint-disable-next-line no-console
-    console.error(err);
-  }
-});
-
-app.use(history());
-app.use(express.static(path.join(__dirname, 'public')));
-
-const port = process.env.PORT || 5000;
-// eslint-disable-next-line no-console
-app.listen(port, () => console.log(`Server started on port ${port}`));
+  const port = process.env.PORT || 5000;
+  // eslint-disable-next-line no-console
+  app.listen(port, () => console.log(`Server started on port ${port}`));
+}
+startServer();
