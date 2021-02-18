dbd818645d93dda987b409b4039a8d183c89986f
Harry Chen <harrychen0314@gmail.com>
2021-02-18 14:12:23 -0800

    add bar chart component

---
+++ client/src/components/BarChartComponent.vue
@@ -0,0 +1,14 @@
+<script>
+import { Bar, mixins } from 'vue-chartjs';
+
+const { reactiveProp } = mixins;
+
+export default {
+  extends: Bar,
+  mixins: [reactiveProp],
+  props: { options: { type: Object, default: null } },
+  mounted() {
+    this.renderChart(this.chartData, this.options);
+  },
+};
+</script>
